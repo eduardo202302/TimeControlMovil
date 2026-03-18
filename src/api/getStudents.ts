4f5756f8a9595e3e2dfa4f267d2af971b4cba9b5
davidliranzo <davidliranzo0@gmail.com>
2026-03-18 15:53:06 -0300

    ultima actualizacion

---
+++ src/api/getStudents.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+
+export const getStudents = async () => {
+  try {
+    // 🔐 obtener datos guardados del login
+    const url = await SecureStore.getItemAsync("urlColegio");
+    const token = await SecureStore.getItemAsync("token");
+
+    // 🚀 petición al backend
+    const response = await axios.get(`${url}/students`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    // 👇 devuelve solo la data
+    return response.data.data;
+
+  } catch (error) {
+    console.log("❌ Error al obtener estudiantes:", error);
+    return [];
+  }
+};
